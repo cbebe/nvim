cba5f849fa3184d81b7abba12e7f3f5f4bd2ccdf
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-12-30 14:35:56 -0700

    Move more configs to their own files

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ if impatient_ok then impatient.enable_profile() end
 for _, source in ipairs {
   "packer-setup",
   "sets",
-  "setup",
+  "setup"
 } do
   local status_ok, fault = pcall(require, source)
   if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
