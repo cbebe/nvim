bd809f7e30a83f27f32dd5beababc00c014034d5
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-12-30 14:11:13 -0700

    oop

--- init.lua
+++ init.lua
@@ -1,7 +1,14 @@
-require 'impatient'
-require 'packer-setup'
-require 'sets'
-require 'setup'
+local impatient_ok, impatient = pcall(require, "impatient")
+if impatient_ok then impatient.enable_profile() end
+
+for _, source in ipairs {
+  "packer-setup",
+  "sets",
+  "setup",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
